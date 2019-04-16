5661dd598809c185daf19bde399ce303d068518b
yjiu <yjiusfgn@163.com>
2019-04-16 09:14:10 +0800

    加密

---
+++ src/test/java/TestJasypt.java
@@ -0,0 +1,24 @@
+
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.App;
+
+
+@RunWith(SpringJUnit4ClassRunner.class) // 让junit与spring环境进行整合
+@SpringBootTest(classes = { App.class }) // 自动加载spring相关的配置文件
+public class TestJasypt {
+	@Autowired
+    StringEncryptor encryptor;
+
+    @Test
+    public void getPass() {
+        String password = encryptor.encrypt("mysqladmin");
+        System.out.println(password+"----------------");
+    }
+}
